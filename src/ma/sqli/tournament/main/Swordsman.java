496cd36546f8762c4e8f01f4ef13447253efc7da
anouarYousfi <anouarsram@gmail.com>
2019-09-10 13:23:06 +0100

    while none isDead the abstract fighters keep engaging

--- src/ma/sqli/tournament/main/Swordsman.java
+++ src/ma/sqli/tournament/main/Swordsman.java
@@ -1,4 +1,22 @@
 package ma.sqli.tournament.main;
 
-public class Swordsman {
+import java.util.logging.Logger;
+
+public class Swordsman extends Fighter {
+
+    public Swordsman() {
+        this.healthPoints = 100;
+        this.damage = 5;
+    }
+
+    @Override
+    public void engage(Fighter fighter) {
+        while (!isSomeoneDead(fighter)) {
+
+            injure(fighter);
+            fighter.engage(this);
+
+        }
+
+    }
 }
--- src/ma/sqli/tournament/main/Viking.java
+++ src/ma/sqli/tournament/main/Viking.java
@@ -1,4 +1,21 @@
 package ma.sqli.tournament.main;
 
-public class Viking {
+public class Viking extends Fighter {
+
+    public Viking() {
+        this.healthPoints = 120;
+        this.damage = 6;
+    }
+
+    @Override
+    public void engage(Fighter fighter) {
+        while(!isSomeoneDead(fighter)) {
+            injure(fighter);
+            fighter.engage(this);
+
+        }
+    }
+
+
 }
+
